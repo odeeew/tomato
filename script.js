82080e4de6d4b62aea9e95248d8427a12c58bfd0
odeeew <odeeew@gmail.com>
2017-07-07 18:12:32 +0800

    修改晚上的時間

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
             $('body').css({'background-image':'url("https://source.unsplash.com/collection/991244/1920x1080")'});
         } else if (now.getHours() > 19 && now.getHours() < 23){
             $('body').css({'background-image':'url("https://source.unsplash.com/collection/991244/1920x1080")'});
-        } else if (now.getHours() === 23){
+        } else if (now.getHours() === 23 || now.getHours() === 0){
             $('body').css({'background-image':'url("https://source.unsplash.com/collection/991244/1920x1080")'});
         } else if (now.getHours() > 0 && now.getHours() < 4){
             $('body').css({'background-image':'url("https://source.unsplash.com/collection/991244/1920x1080")'});
